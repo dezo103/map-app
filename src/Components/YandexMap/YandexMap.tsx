c45a4155b9384f9264ccd7910ee7b9fc80c1f1fd
Aliaksandr Rudyk <dezo103@gmail.com>
2022-02-15 00:12:40 +0300

    fix some errors

--- src/Components/YandexMap/YandexMap.tsx
+++ src/Components/YandexMap/YandexMap.tsx
@@ -3,12 +3,18 @@ import {YMaps, Map} from 'react-yandex-maps';
 import styles from './YandexMap.module.css'
 
 const YandexMap = () => {
+
+    const onClickHandler = (ev:any) => {
+        console.log(ev)
+    }
+
     return (
         <YMaps className={styles.yandexMap}>
             <Map
-                defaultState={{center: [55.75, 37.57], zoom: 9}}
+                defaultState={{center: [53.902284, 27.34], zoom: 10}}
                 width={"100vw"}
                 height={"100vh"}
+                onClick={(e: any)=>{onClickHandler(e)}}
             />
         </YMaps>
     );
