1ec67da355268d0b3c4918d44a3e99329006ca2c
Aliaksandr Rudyk <dezo103@gmail.com>
2022-02-20 22:25:36 +0300

    add .env APIKEY

--- src/api/yandexMapAPI.ts
+++ src/api/yandexMapAPI.ts
@@ -6,6 +6,8 @@ const instance = axios.create({
 
 const apikey = '37a700cd-2692-4c88-9cb8-965eabbaea52'
 
+// const {REACT_APP_APIKEY} = process.env
+
 export const yandexMapAPI = {
     getAddressByCoordinates(location: any) {
         return instance.get(`?apikey=${apikey}&format=json&geocode=${location}`)
@@ -13,6 +15,12 @@ export const yandexMapAPI = {
     gedCoordinateByAddress(address: any) {
         return instance.get(`?apikey=${apikey}&format=json&geocode=${address}`)
     }
+    // getAddressByCoordinates(location: any) {
+    //     return instance.get(`?apikey=${REACT_APP_APIKEY}&format=json&geocode=${location}`)
+    // },
+    // gedCoordinateByAddress(address: any) {
+    //     return instance.get(`?apikey=${REACT_APP_APIKEY}&format=json&geocode=${address}`)
+    // }
 }
 
 // https://geocode-maps.yandex.ru/1.x/?apikey=ваш API-ключ&geocode=37.611347,55.760241
